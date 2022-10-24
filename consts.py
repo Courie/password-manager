1e2172f27698452095fc96ec3327974d63b8cdf5
Courie <laurisjump@gmail.com>
2022-10-24 18:59:48 +0300

    uhhh oops

---
+++ consts.py
@@ -0,0 +1,28 @@
+MENU_TEXT = '''\n(n) Add new password
+(r) Retrieve password
+(d) Delete password
+(c) Change password
+(l) List all
+(q) Quit\nChoose an option: '''
+
+SUPER_START_TEXT = """                                 _                           _  _   
+
+                                   _____       
+  _____ ___.__.      ___________ _/ ____\____  
+ /     <   |  |     /  ___/\__  \\   __\/ __ \ 
+|  Y Y  \___  |     \___ \  / __ \|  | \  ___/ 
+|__|_|  / ____| /\ /____  >(____  /__|  \___  >
+      \/\/      \/      \/      \/          \/                            
+"""
+
+REGISTRATION_SUCCESS_TEXT = "You've registered successfully !!!"
+
+REGISTRATION_FAILED_TEXT = "Master Password doesn't match, try again..."
+
+VERIFICATION_FAILED_TEXT = "Wrong Password, Try again..."
+
+DELETED_TEXT = "Deleted successfully..."
+
+CHANGED_TEXT = "Password changed successfully..."
+
+WRONG_INPUT_TEXT = "Please provide correct input..."
